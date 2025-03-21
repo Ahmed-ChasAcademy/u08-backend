ff899c602d4e6efcee5df3f015cab3e2c9698c6e
Ahmed <ahmed.bahobeshi@chasAcademy.se>
2025-03-21 08:51:33 +0100

    testing api requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,26 +1,30 @@
 import express from 'express';
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
-    console.log(`Running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Running on http://localhost:${PORT}`);
 });
 
-app.get('/', (request, response) => {
-    response.status(200).send({message: "hello"})
-})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "hello" });
+});
 
-app.get('/api/users', (req,res) => {
-    res.send([{
-        id: 1,
-        name: "Ahmed",
-        email: "Ahmed@gmai.commmm"
-    }])
-})
+app.get('/api/users', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "Ahmed",
+            email: "Ahmed@gmai.commmm"
+        }
+    ]);
+});
 
+app.get('/pets/:id', (req, res) => {
+    console.log(req.params);
+    const parsedId = parseInt(req.params.id);
+    if (isNaN(parsedId)) return res.status(400).send({ msg: "bad request" });
 
-app.get('/pets/:id', (req,res) =>{
-    console.log(req.params)
-})
+    res.send({ id: parsedId, msg: "Pet found" });
+});
